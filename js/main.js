bd517ed9d98211ac9b5b6bb7a9aef3dfc54f6a35
Taoavatara <156911166+Taoavatara@users.noreply.github.com>
2024-12-13 15:20:31 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U, BABYY').split('')
+    const titles = ('I LOVE U, IFAA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
